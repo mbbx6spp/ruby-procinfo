5e1b0d642ca4423357a4983a4089ecbde40ff5c5
Susan Potter <me@susanpotter.net>
2013-08-18 23:52:46 -0500

    Update API to return SELF or CHILDREN rusage stats

--- lib/procinfo.rb
+++ lib/procinfo.rb
@@ -1,1 +1,18 @@
 require 'procinfo/procinfo'
+
+module Process
+  def self.stats(type = :self)
+    case type
+    when :self
+      stats_for_self
+    when :children
+      stats_for_children
+    else
+      raise ArgumentError.new("Unknown type: #{type}")
+    end
+  end
+
+  class << self
+    private :stats_for_self, :stats_for_children
+  end
+end
